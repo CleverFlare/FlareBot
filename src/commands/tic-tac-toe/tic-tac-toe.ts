154a1f40aaa139d4418fb3cfe92459ad022d9e90
Muhammad Maher <blastclever@gmail.com>
2024-09-20 07:25:33 +0300

    feat: add the tic tac toe game

--- src/commands/tic-tac-toe/tic-tac-toe.ts
+++ src/commands/tic-tac-toe/tic-tac-toe.ts
@@ -1,11 +1,18 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ChatInputCommandInteraction,
+  ComponentType,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import { constructBoardFromBinary } from "./utils/construct-board-from-binary";
+import { createChoicesButtons } from "./utils/create-buttons-from-binary";
 import { detectWinner } from "./utils/detect-winner";
 
 interface Game {
   player: string;
   opponent: string | "NPC";
-  board: [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]];
   availableMoves: number;
   playerMoves: number;
   opponentMoves: number;
@@ -29,16 +36,191 @@ export default {
     .setDescription(
       "Play with the computer or with your friend the famous Tic Tac Toe game.",
     )
-    .addBooleanOption((option) =>
+    .addUserOption((option) =>
       option
-        .setName("multiplayer")
-        .setDescription("If set to True, you'll start a game with a friend"),
+        .setName("opponent")
+        .setDescription("Choose your opponent.")
+        .setRequired(true),
     ),
   async execute(interaction: ChatInputCommandInteraction) {
     try {
-      const winner = detectWinner(0b000000000, 0b000000000, winningCombos);
-      if (winner) interaction.reply(winner);
-      else interaction.reply("tie");
+      let gameOver = false;
+
+      let game = games.find(
+        (game) =>
+          game.player === interaction.user.id ||
+          game.opponent === interaction.user.id,
+      );
+
+      if (!game) {
+        games.push({
+          player: interaction.user.id,
+          opponent: interaction.options.getUser("opponent")?.id ?? "NPC",
+          opponentMoves: 0b000000000,
+          playerMoves: 0b000000000,
+          get availableMoves() {
+            return this.playerMoves | this.opponentMoves;
+          },
+          turn: true,
+        });
+
+        game = games.at(-1)!;
+      }
+
+      console.log("GAMES:", games);
+
+      let userTurn = game.turn ? game.player : game.opponent;
+
+      const initialEmbed = new EmbedBuilder()
+        .setTitle("Tic Tac Toe Game")
+        .setDescription(
+          `A game started between <@${game.player}> and ${game.opponent === "NPC" ? "the computer" : `<@${game.opponent}>`}`,
+        )
+        .setColor("Blue");
+
+      const boardEmbed = new EmbedBuilder()
+        .setDescription(
+          `Turn: <@${userTurn}>\n${constructBoardFromBinary(
+            game.playerMoves,
+            game.opponentMoves,
+          )}`,
+        )
+        .setColor("Blue");
+
+      let buttons = createChoicesButtons();
+
+      let actionRows = [
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          buttons.slice(0, 3),
+        ),
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          buttons.slice(3, 6),
+        ),
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          buttons.slice(6, 9),
+        ),
+      ];
+
+      const reply = await interaction.reply({
+        embeds: [initialEmbed, boardEmbed],
+        components: [...actionRows],
+        fetchReply: true,
+      });
+
+      const collector = reply.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        filter: (i) => i.user.id === userTurn || !gameOver,
+        time: 30_000,
+      });
+
+      collector.on("collect", async (interaction) => {
+        if (interaction.user.id !== userTurn) {
+          if (interaction.user.id === game.opponent) {
+            interaction.reply({
+              content: "It's not your turn yet!",
+              ephemeral: true,
+            });
+          } else {
+            interaction.reply({
+              content:
+                "You're not a party in this game. Feel free to begin a new one if you like.",
+              ephemeral: true,
+            });
+          }
+
+          return;
+        }
+
+        if (
+          (game.availableMoves | parseInt(interaction.customId, 2)) ===
+          game.availableMoves
+        ) {
+          interaction.reply({
+            content:
+              "This position is occupied! Try playing in another position.",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        if (userTurn === game.player) {
+          game.playerMoves |= parseInt(interaction.customId, 2);
+        } else if (userTurn === game.opponent) {
+          game.opponentMoves |= parseInt(interaction.customId, 2);
+        }
+
+        game.turn = !game.turn;
+
+        userTurn = game.turn ? game.player : game.opponent;
+
+        const winner = detectWinner(
+          game.playerMoves,
+          game.opponentMoves,
+          winningCombos,
+        );
+
+        if (0b111111111 === game.availableMoves && !winner) {
+          boardEmbed.setDescription(
+            `${constructBoardFromBinary(game.playerMoves, game.opponentMoves)}`,
+          );
+
+          initialEmbed.setDescription(
+            `A game started between <@${game.player}> and ${game.opponent === "NPC" ? "the computer" : `<@${game.opponent}>`}\nResult: Tie`,
+          );
+
+          await reply.edit({
+            embeds: [initialEmbed, boardEmbed],
+            components: [],
+          });
+
+          await interaction.deferUpdate();
+
+          const gameIndex = games.indexOf(game);
+
+          games.splice(gameIndex, 1);
+
+          return;
+        }
+
+        if (!winner) {
+          boardEmbed.setDescription(
+            `Turn: <@${userTurn}>\n${constructBoardFromBinary(
+              game.playerMoves,
+              game.opponentMoves,
+            )}`,
+          );
+
+          await reply.edit({
+            embeds: [initialEmbed, boardEmbed],
+            components: [...actionRows],
+          });
+        } else {
+          boardEmbed
+            .setDescription(
+              `${constructBoardFromBinary(game.playerMoves, game.opponentMoves)}`,
+            )
+            .setColor("Green");
+
+          initialEmbed
+            .setDescription(
+              `A game started between <@${game.player}> and ${game.opponent === "NPC" ? "the computer" : `<@${game.opponent}>`}\nWinner: <@${winner === "player" ? game.player : game.opponent}>`,
+            )
+            .setColor("Green");
+
+          await reply.edit({
+            embeds: [initialEmbed, boardEmbed],
+            components: [],
+          });
+
+          const gameIndex = games.indexOf(game);
+
+          games.splice(gameIndex, 1);
+        }
+
+        await interaction.deferUpdate();
+
+        console.log("Game's Current State:", game);
+      });
     } catch (err) {
       console.log("ERROR:", err);
       interaction.reply(
---
+++ src/commands/tic-tac-toe/utils/create-buttons-from-binary.ts
@@ -0,0 +1,60 @@
+import { ButtonBuilder, ButtonStyle } from "discord.js";
+
+type Button = {
+  emoji: string;
+  value: number;
+};
+
+const buttons: Button[] = [
+  {
+    emoji: "1️⃣",
+    value: 0b100000000,
+  },
+  {
+    emoji: "2️⃣",
+    value: 0b010000000,
+  },
+  {
+    emoji: "3️⃣",
+    value: 0b001000000,
+  },
+  {
+    emoji: "4️⃣",
+    value: 0b000100000,
+  },
+  {
+    emoji: "5️⃣",
+    value: 0b000010000,
+  },
+  {
+    emoji: "6️⃣",
+    value: 0b000001000,
+  },
+  {
+    emoji: "7️⃣",
+    value: 0b000000100,
+  },
+  {
+    emoji: "8️⃣",
+    value: 0b000000010,
+  },
+  {
+    emoji: "9️⃣",
+    value: 0b000000001,
+  },
+];
+
+export function createChoicesButtons() {
+  const results = [];
+
+  for (const button of buttons) {
+    results.push(
+      new ButtonBuilder()
+        .setCustomId(button.value.toString(2).padStart(9, "0"))
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji(button.emoji),
+    );
+  }
+
+  return results;
+}
